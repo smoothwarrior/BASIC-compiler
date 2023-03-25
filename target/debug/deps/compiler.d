1c885f330805008cc9c5819cfb4c6148382dc653
smoothwarrior <57007298+smoothwarrior@users.noreply.github.com>
2023-03-25 17:23:12 -0500

    Delete compiler.d

--- target/debug/deps/compiler.d
+++
@@ -1,5 +0,0 @@
-C:\Users\wildb\Documents\Projects\rust_playground\compiler\compiler\target\debug\deps\compiler.exe: src\main.rs
-
-C:\Users\wildb\Documents\Projects\rust_playground\compiler\compiler\target\debug\deps\compiler.d: src\main.rs
-
-src\main.rs:
